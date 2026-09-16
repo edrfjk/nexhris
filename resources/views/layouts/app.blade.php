d803ff5dfe1f75900211b89ce2bd5bfef7a9db86
edrfjk <edrfjk@gmail.com>
2026-09-17 00:05:43 +0800

    Icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,7 @@
     <meta charset="UTF-8">
     <title>@yield('title', 'NexHRIS') | ISPSC Tagudin</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="/favicon.png">
     {{-- Alpine is bundled into resources/js/app.js. Loading it from a CDN as
          well would start it twice. --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
